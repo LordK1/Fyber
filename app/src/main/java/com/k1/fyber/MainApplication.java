90cfc0165b0a57ae9043fd6321986cb68a180a23
K1 <keyvan.ghanizadeh@gmail.com>
2016-08-23 22:24:21 +0430

    intial commit

---
+++ app/src/main/java/com/k1/fyber/MainApplication.java
@@ -0,0 +1,14 @@
+package com.k1.fyber;
+
+import android.app.Application;
+
+/**
+ * Created by K1 on 8/23/16.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
